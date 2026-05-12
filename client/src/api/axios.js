7958784401c36d3d4079a84dd0316e8e35531f36
Abinaya V <abinaya7247@gmail.com>
2026-05-12 15:21:24 +0530

    fix API baseURL

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-//const API_URL = "http://localhost:5000/api";
-const API = "https://snap-talk-3-bl2l.onrender.com";
+const API_URL = "https://snap-talk-3-bl2l.onrender.com/api";
 
 const axiosInstance = axios.create({
   baseURL: API_URL,
@@ -10,27 +9,19 @@ const axiosInstance = axios.create({
   },
 });
 
-// Add token to requests
-axiosInstance.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem("token");
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
+axiosInstance.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
   }
-);
+  return config;
+});
 
-// Handle responses
 axiosInstance.interceptors.response.use(
   (response) => response,
   (error) => {
     if (error.response?.status === 401) {
-      localStorage.removeItem("token");
-      localStorage.removeItem("user");
+      localStorage.clear();
       window.location.href = "/";
     }
     return Promise.reject(error);
