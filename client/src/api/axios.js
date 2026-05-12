09c7686a0575cb1533342dfbda9adcd9306ea68f
Abinaya V <abinaya7247@gmail.com>
2026-05-12 12:41:09 +0530

    updated backend url

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api";
+//const API_URL = "http://localhost:5000/api";
+const API = "https://snap-talk-3-bl2l.onrender.com";
 
 const axiosInstance = axios.create({
   baseURL: API_URL,
