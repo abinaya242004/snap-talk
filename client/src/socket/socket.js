ab9d6d29f3217f639be231777ac0aab7fe8e16db
Abinaya V <abinaya7247@gmail.com>
2026-05-12 14:01:04 +0530

    fix socket connection and backend deployment

--- client/src/socket/socket.js
+++ client/src/socket/socket.js
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 
-const SOCKET_URL = "http://localhost:5000";
-
+//const SOCKET_URL = "http://localhost:5000";
+const SOCKET_URL = "https://snap-talk-3-bl2l.onrender.com";
 const socket = io(SOCKET_URL);
 
 export default socket;
